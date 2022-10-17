2bd12e07d3a880bef156bbef118ca7e3c1a5528a
michvdh <michael.v.d.hingpit@gmail.com>
2022-10-17 18:32:46 +0800

    added Cart Totals in cart page

--- pages/cart/CartTotals.tsx
+++ pages/cart/CartTotals.tsx
@@ -1,8 +1,34 @@
 import classes from './CartTotals.module.scss';
+import { useSelector } from 'react-redux';
 
 const CartTotals = () => {
+  const cartTotalPrice = useSelector(
+    (state: { cart: { totalPrice: number } }) => state.cart.totalPrice
+  );
+
   return (
-    <div></div>
+    <div className={classes['cart-totals']}>
+      <div className={classes['totals-table']}>
+        <ul>
+          <li className={classes.full}>Cart Totals</li>
+          <li className={classes.divided}>
+            <span>Subtotal</span>
+            <span>{cartTotalPrice.toFixed(2)}</span>
+          </li>
+          <li className={classes.divided}>
+            <span>Shipping</span>
+            <span>Calculate Shipping</span>
+          </li>
+          <li className={classes.divided}>
+            <span>Total</span>
+            <span>{cartTotalPrice.toFixed(2)}</span>
+          </li>
+          <li className={classes.full}>
+            <button>Proceed to checkout</button>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
