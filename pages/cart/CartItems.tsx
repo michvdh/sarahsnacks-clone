ad8bebea1dc81aaaf73341e9ac723889bd82d90f
michvdh <michael.v.d.hingpit@gmail.com>
2022-10-18 11:14:35 +0800

    updated cart view when empty

--- pages/cart/CartItems.tsx
+++ pages/cart/CartItems.tsx
@@ -2,7 +2,7 @@ import classes from "./CartItems.module.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../../store/cart";
 import { MutableRefObject, RefObject, useEffect, useRef } from "react";
-import {useState} from 'react';
+import { useState } from "react";
 
 interface cartItemsInterface {
   id: string;
@@ -31,10 +31,9 @@ const CartItems = () => {
   // console.log(refArray[0].current?.value);
 
   ////////////////////////////////////////
-  
-  const [inputId, setInputId] = useState(''); // id of the specific item being adjusted
-  const [inputQty, setInputQty] = useState(0); // quantity of the specific item being adjusted
 
+  const [inputId, setInputId] = useState(""); // id of the specific item being adjusted
+  const [inputQty, setInputQty] = useState(0); // quantity of the specific item being adjusted
 
   const incrementQty = (id: string) => {
     const itemIndex = cartItems.findIndex((p) => p.id === id);
@@ -49,12 +48,12 @@ const CartItems = () => {
     if (cartItems[itemIndex].qty >= 1) {
       setInputQty(cartItems[itemIndex].qty - 1);
     }
-  }
+  };
 
   const inputChangeHandler = (e) => {
     setInputId(e.target.id);
     setInputQty(e.target.value);
-  }
+  };
 
   // removed delay - useEffect is now purely used for dispatch
   useEffect(() => {
@@ -63,60 +62,70 @@ const CartItems = () => {
     // }, 0);
     // return () => clearTimeout(timer);
 
-    dispatch(cartActions.adjustItemQty({inputId: inputId, inputQty: +inputQty}));
-  }, [inputQty, inputId])
-
+    dispatch(
+      cartActions.adjustItemQty({ inputId: inputId, inputQty: +inputQty })
+    );
+  }, [inputQty, inputId]);
 
   const removeItemHandler = (id: string) => {
-    dispatch(cartActions.removeItem({inputId: id}))
-  }
+    dispatch(cartActions.removeItem({ inputId: id }));
+  };
 
   return (
     <div className={`${classes["cart-items"]}`}>
-      {cartItems.length === 0 && (
+      {/* {cartItems.length === 0 && (
         <div>
           <span>(icon here)</span>
           <span>YOUR CART IS CURRENTLY EMPTY.</span>
         </div>
-      )}
+      )} */}
 
       {/* add a different JSX block for mobile view. This one below is for desktop / large tablet view */}
-      {cartItems.length > 0 && (
-        <ul>
-          <li className={classes.row}>
-            <div></div>
-            <div></div>
+
+      <ul>
+        <li className={classes.row}>
+          <div></div>
+          <div></div>
+          <div>
+            <h3>Product</h3>
+          </div>
+          <div>
+            <h3>Price</h3>
+          </div>
+          <div>
+            <h3>Quantity</h3>
+          </div>
+          <div>
+            <h3>Subtotal</h3>
+          </div>
+        </li>
+        {cartItems.map((item: cartItemsInterface, index) => (
+          <li key={index} className={classes.row}>
             <div>
-              <h3>Product</h3>
+              <button onClick={() => removeItemHandler(item.id)}>x</button>
             </div>
+            <div>image</div>
             <div>
-              <h3>Price</h3>
+              {item.productName} {item.varSize && `- ${item.varSize}`}
             </div>
-            <div>
-              <h3>Quantity</h3>
+            <div>{item.varPrice}</div>
+            <div className={`${classes.quantity}`}>
+              <button onClick={() => decrementQty(item.id)}>-</button>
+              <input
+                id={item.id}
+                type="number"
+                min={0}
+                value={item.qty}
+                onChange={inputChangeHandler}
+              />
+              <button onClick={() => incrementQty(item.id)}>+</button>
             </div>
-            <div>
-              <h3>Subtotal</h3>
-            </div>
+            <div>{item.varPrice * item.qty}</div>
           </li>
-          {cartItems.map((item: cartItemsInterface, index) => (
-            <li key={index} className={classes.row}>
-              <div><button onClick={() => removeItemHandler(item.id)}>x</button></div>
-              <div>image</div>
-              <div>{item.productName} {item.varSize && `- ${item.varSize}`}</div>
-              <div>{item.varPrice}</div>
-              <div className={`${classes.quantity}`}>
-                <button onClick={() => decrementQty(item.id)}>-</button>
-                <input id={item.id} type="number" min={0} value={item.qty} onChange={inputChangeHandler} />
-                <button onClick={() => incrementQty(item.id)}>+</button>
-              </div>
-              <div>{item.varPrice * item.qty}</div>
-            </li>
-          ))}
-        </ul>
-      )}
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default CartItems;
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,16 +1,42 @@
-import classes from './Cart.module.scss';
-import CartItems from './CartItems';
-import CartTotals from './CartTotals';
+import classes from "./Cart.module.scss";
+import CartItems from "./CartItems";
+import CartTotals from "./CartTotals";
+import { useSelector } from "react-redux";
+import { Fragment } from "react";
+
+interface cartItemsInterface {
+  id: string;
+  productName: string;
+  varPrice: number; // variation price
+  varSize: string; // variation size
+  qty: number;
+}
 
 const Cart = () => {
+  const cartItems = useSelector(
+    (state: { cart: { cartItems: cartItemsInterface[] } }) =>
+      state.cart.cartItems
+  );
+
   return (
     <main className={`main`}>
       <section className={`${classes.cart} cart`}>
-        <CartItems />
-        <CartTotals />
+        {cartItems.length === 0 && (
+          <div>
+            <span>(icon here)</span>
+            <span>YOUR CART IS CURRENTLY EMPTY.</span>
+          </div>
+        )}
+
+        {cartItems.length > 0 && (
+          <Fragment>
+            <CartItems />
+            <CartTotals />
+          </Fragment>
+        )}
       </section>
-    </main> 
-  )
-}
+    </main>
+  );
+};
 
 export default Cart;
