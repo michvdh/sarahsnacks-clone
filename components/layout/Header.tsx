29edd7d6057c71a936f12686af68b456a92d0fba
michvdh <michael.v.d.hingpit@gmail.com>
2022-11-25 15:39:35 +0800

    Fixed cart icon. Fixed nav close when clicking on nav

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -43,6 +43,10 @@ const Header: React.FC = () => {
     setChecked(e.target.checked);
   }
 
+  const mobileNavHandler = () => {
+    setChecked(false); 
+  }
+
 
   useEffect(() => {
     let cartLS =
@@ -87,18 +91,20 @@ const Header: React.FC = () => {
           <Link href={{ pathname: `/cart` }} passHref>
             <a>
               <div className={`${classes['cart__link']}`}>
-                <span>
+                <span className={classes.icons}>
                   <FontAwesomeIcon
                     className={`${classes[`fa-icon`]} fa-icon--left`}
                     icon={faBagShopping}
                   />
-                  <FontAwesomeIcon
-                    className={`${classes[`fa-icon`]} fa-icon--green`}
-                    icon={faCaretLeft}
-                  />
-                  <span className={`${classes["cart__count"]} cart__count`}>
-                    {cartTotalQty}
-                  </span>
+                  <div className={`${classes["cart__count"]} cart__count`}>
+                    <FontAwesomeIcon
+                    className={`${classes['fa-icon--caret']} fa-icon--green`}
+                    icon={faCaretLeft} />
+
+                    <span className={`${classes['total-qty']}`}>      
+                      {cartTotalQty}
+                    </span>
+                  </div>
                 </span>
                 <span className={`${classes['cart__price']} `}>${cartTotalPrice.toFixed(2)}</span>
               </div>
@@ -110,7 +116,7 @@ const Header: React.FC = () => {
         <div className={classes.border}></div>
 
         <div className={classes.hamburger}>
-          <input type="checkbox" className={classes.checkbox} id="navi-toggle" onChange={checkboxHandler} />
+          <input type="checkbox" className={classes.checkbox} id="navi-toggle" onChange={checkboxHandler} checked={checked}/>
           <label htmlFor="navi-toggle" className={classes['mobile-nav']}>
             <span className={classes['mobile-nav__icon']}>&nbsp;</span>
           </label> 
@@ -118,29 +124,35 @@ const Header: React.FC = () => {
 
         <div className={`${classes.nav} nav ${checked ? classes.open : classes.closed}`}>
           <ul className={``}>
-            <li>
+            <li onClick={mobileNavHandler}>
               <Link href="/">
                 <a className={`${classes.link}`}>Home</a>
               </Link>
             </li>
-            <li>
+
+            <li onClick={mobileNavHandler}>
               <Link href="/shop">
                 <a className={`${classes.link}`}>Shop</a>
               </Link>
             </li>
-            <li>
+
+            <li onClick={mobileNavHandler}>
               <a className={`${classes.link}`}>Our Story</a>
             </li>
-            <li>
+
+            <li onClick={mobileNavHandler}>
               <a className={`${classes.link}`}>Where to buy</a>
             </li>
-            <li>
+
+            <li onClick={mobileNavHandler}>
               <a className={`${classes.link}`}>Recipes</a>
             </li>
-            <li>
+
+            <li onClick={mobileNavHandler}>
               <a className={`${classes.link}`}>News</a>
             </li>
-            <li>
+            
+            <li onClick={mobileNavHandler}>
               <a className={`${classes.link}`}>Contact</a>
             </li>
           </ul>
