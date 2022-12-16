41f0330ecc73b0d61afcff165e579b24d77b0efe
michvdh <michael.v.d.hingpit@gmail.com>
2022-12-16 14:53:51 +0800

    fixed devtools error for additionalInfo

--- components/ui/ProductCompleteDetails/AdditionalInfo.tsx
+++ components/ui/ProductCompleteDetails/AdditionalInfo.tsx
@@ -29,50 +29,49 @@ const AdditionalInfo: React.FC<AdditionalInfoProps> = (props) => {
         {/* warning, ingredients, sizes -- these values are static */}
         <table className={classes.table}>
 
-          {/* weight and dimensions */}
-          {props.selectionStatus && (
-            <Fragment>
+          <tbody>
+            {/* weight and dimensions */}
+            {props.selectionStatus && (
+              <Fragment>
+                <tr className={`${classes.row}`}>
+                  <th className={`${classes['category']}`}>Weight</th>
+                  <td className={`${classes['data']}`}>{props.variations[props.vIndex].weight}</td>
+                </tr>
+                <tr className={`${classes.row}`}>
+                  <th className={`${classes['category']}`}>Dimensions</th>
+                  <td className={`${classes['data']}`}>{props.variations[props.vIndex].dimensions}</td>
+                </tr>
+              </Fragment>
+            )}
+            {/* Warning */}
+            {props.additionalInfo.warning !== "" && (
               <tr className={`${classes.row}`}>
-                <th className={`${classes['category']}`}>Weight</th>
-                <td className={`${classes['data']}`}>{props.variations[props.vIndex].weight}</td>
+                <th className={`${classes['category']}`}>Warning</th>
+                <td className={`${classes['data']}`}>{props.additionalInfo.warning}</td>
               </tr>
+            )}
+            {/* Ingredients */}
+            {props.additionalInfo.ingredients !== "" && (
               <tr className={`${classes.row}`}>
-                <th className={`${classes['category']}`}>Dimensions</th>
-                <td className={`${classes['data']}`}>{props.variations[props.vIndex].dimensions}</td>
+                <th className={`${classes['category']}`}>Ingredients</th>
+                <td className={`${classes['data']}`}>{props.additionalInfo.ingredients}</td>
               </tr>
-            </Fragment>
-          )}
-
-          {/* Warning */}
-          {props.additionalInfo.warning !== "" && (
-            <tr className={`${classes.row}`}>
-              <th className={`${classes['category']}`}>Warning</th>
-              <td className={`${classes['data']}`}>{props.additionalInfo.warning}</td>
-            </tr>
-          )}
-
-          {/* Ingredients */}
-          {props.additionalInfo.ingredients !== "" && (
-            <tr className={`${classes.row}`}>
-              <th className={`${classes['category']}`}>Ingredients</th>
-              <td className={`${classes['data']}`}>{props.additionalInfo.ingredients}</td>
-            </tr>
-          )}
-
-          {/* Sizes */}
-          {props.variations[props.vIndex].size !== "" &&
-            <tr className={`${classes.row}`}>
-              <th className={`${classes['category']}`}>Sizes</th>
-              <td className={`${classes['data']}`}>
-                {props.variations.map((variation, index) => (
-                  <span>
-                    {variation.size}
-                    {index !== props.variations.length - 1 ? ", " : ""}
-                  </span>
-                ))}
-              </td>
-            </tr>
-          }
+            )}
+            {/* Sizes */}
+            {props.variations[props.vIndex].size !== "" &&
+              <tr className={`${classes.row}`}>
+                <th className={`${classes['category']}`}>Sizes</th>
+                <td className={`${classes['data']}`}>
+                  {props.variations.map((variation, index) => (
+                    <span key={index}>
+                      {variation.size}
+                      {index !== props.variations.length - 1 ? ", " : ""}
+                    </span>
+                  ))}
+                </td>
+              </tr>
+            }
+          </tbody>
         </table>
       </div>
     </Fragment>
