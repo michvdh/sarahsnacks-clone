f8a74ad9e766ceed6d667d8874808546311698c9
michvdh <michael.v.d.hingpit@gmail.com>
2022-12-17 17:32:26 +0800

    fixed 404 Head

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,18 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
 export default function Custom404() {
   return (
-    <main className={`main`}>
-      <section className={`product`}>
-        <h1>404 - Page Not Found</h1>
-      </section>
-    </main>
+    <Fragment>
+      <Head>
+        <title>Page Not Found - Sarah&apos;s Snacks</title>
+        <link rel="shortcut icon" href="/sarahsnacks-fav.png" />
+      </Head>
+      <main className={`main`}>
+        <section className={`product`}>
+          <h1>404 - Page Not Found</h1>
+        </section>
+      </main>
+    </Fragment>
   );
 }
